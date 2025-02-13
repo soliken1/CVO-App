1a33bf8a79d1d45a416a2cde7ca3b721e5503297
soliken <kennethrex456@gmail.com>
2025-02-13 14:32:35 +0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <label className="font-bold text-6xl">Welcome to CVO</label>
+    </div>
+  );
+}
+
+export default App;
